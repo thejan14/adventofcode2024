a39cb8f250bb29a2e06034ff89e7e18318cd691e
thejan14 <jan.oberpichler@googlemail.com>
2024-12-03 17:35:22 +0100

    solution day 3

---
+++ day03/part1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <regex>
+
+#include "aoc.h"
+
+const std::regex mulRegex = std::regex(R"(mul\((\d+),(\d+)\))");
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto answer = 0;
+    std::smatch match;
+    auto it = input.cbegin();
+    while (regex_search(it, input.cend(), match, mulRegex))
+    {
+        answer += stoi(match[1]) * stoi(match[2]);
+        it = match.suffix().first - 1;
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
---
+++ day03/part2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <regex>
+
+#include "aoc.h"
+
+const std::regex mulRegex = std::regex(R"(mul\((\d+),(\d+)\)|(do|don't)\(\))");
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    auto answer = 0;
+    auto multEnabled = true;
+    std::smatch match;
+    auto it = input.cbegin();
+    while (regex_search(it, input.cend(), match, mulRegex))
+    {
+        if (match[3].matched)
+        {
+            multEnabled = match[3] == "do";
+        }
+        else if (multEnabled)
+        {
+            answer += stoi(match[1]) * stoi(match[2]);
+        }
+
+        it = match.suffix().first - 1;
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
