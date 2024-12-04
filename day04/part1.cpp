17259ad916c83db68f26913da6c85faebc9be893
thejan14 <jan.oberpichler@googlemail.com>
2024-12-04 16:22:42 +0100

    solution day 4

---
+++ day04/part1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+
+#include "aoc.h"
+
+constexpr std::array<char, 4> term{ 'X', 'M', 'A', 'S' };
+constexpr std::array<std::pair<int, int>, 8> directions
+{
+    { {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1} }
+};
+
+std::vector<std::vector<char>> map;
+
+// (sx, sy) => start position
+// (dx, dy) => direction vector
+bool checkDirection(int sx, int sy, int const dx, int const dy)
+{
+    if (map[sy][sx] != term[0])
+    {
+        return false;
+    }
+
+    for (auto i = 1; i < 4; i++)
+    {
+        sx += dx;
+        sy += dy;
+        if (sy < 0
+            || sy > map.size() - 1
+            || sx < 0
+            || sx > map[sy].size() - 1
+            || map[sy][sx] != term[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    for (auto const line : input | std::views::split('\n'))
+    {
+        auto const row = std::string_view{ line };
+        map.emplace_back(row.begin(), row.end());
+    }
+
+    auto answer = 0;
+    for (auto y = 0ULL; y < map.size(); y++)
+    {
+        for (auto x = 0ULL; x < map[y].size(); x++)
+        {
+            for (auto const& [dx, dy] : directions)
+            {
+                answer += checkDirection(x, y, dx, dy);
+            }
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
---
+++ day04/part2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
+#include <ranges>
+
+#include "aoc.h"
+
+std::vector<std::vector<char>> map;
+
+bool checkPair(char const a, char const b)
+{
+    if (a == 'M')
+    {
+        return b == 'S';
+    }
+
+    if (a == 'S')
+    {
+        return b == 'M';
+    }
+
+    return false;
+}
+
+int main()
+{
+    auto input = aoc::readInput();
+    const auto execStart = std::chrono::high_resolution_clock::now();
+
+    /* begin solution */
+
+    for (auto const line : input | std::views::split('\n'))
+    {
+        auto const row = std::string_view{ line };
+        map.emplace_back(row.begin(), row.end());
+    }
+
+    auto answer = 0;
+    for (auto y = 1ULL; y < map.size() - 1; y++)
+    {
+        for (auto x = 1ULL; x < map[y].size() - 1; x++)
+        {
+            if (map[y][x] == 'A')
+            {
+                answer += checkPair(map[y - 1][x - 1], map[y + 1][x + 1])
+                    && checkPair(map[y + 1][x - 1], map[y - 1][x + 1]);
+            }
+        }
+    }
+
+    /* end solution */
+
+    const auto execEnd = std::chrono::high_resolution_clock::now();
+    const auto micros = std::chrono::duration_cast<std::chrono::microseconds> (execEnd - execStart);
+    std::cout << std::format("{} ({})", answer, micros);
+}
